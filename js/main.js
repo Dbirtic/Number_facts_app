1f7c2bdf180791f59f1a794b7091944a2b5ca465
Dbirtic <Dominik.birtic666@gmail.com>
2021-04-14 21:07:02 +0200

    finished this short project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-// Write your code here...
+// Get the fact ids
+let fact = document.querySelector('#fact');
+let factText = document.querySelector('#factText');
+
+// Get the number that is input
+let numberInput = document.querySelector('#numberInput');
+// put an event listener on the number input
+numberInput.addEventListener('input', getFactFetch);
+
+// Fetch number fact using Ajax or XHR
+function getFactAjax(){
+    let number = numberInput.value;
+    if(number != ''){
+        let xhr = new XMLHttpRequest();
+        // create a GET request to the numbers api site for the input number
+        xhr.open('GET', 'http://numbersapi.com/' + number);
+
+        xhr.onload = function(){
+            if(this.status == 200){
+                fact.style.display = 'block';
+                factText.innerText = this.responseText;
+            }
+        }
+
+        xhr.send();
+    }
+}
+
+// Using the fetch api
+function getFactFetch(){
+    let number = numberInput.value;
+
+    if( number != ''){
+        fetch('http://numbersapi.com/' + number)
+        .then(response => response.text())
+        .then(data => {
+            fact.style.display = 'block';
+            factText.innerText = data;
+        })
+        .catch(err => console.log(err));
+    }
+}
